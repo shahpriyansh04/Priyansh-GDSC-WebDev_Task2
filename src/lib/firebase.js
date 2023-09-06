4360478eb98e90c53320fbd82580a63798c9e119
Priyansh Shah <shah.priyansh04@gmail.com>
2023-09-06 12:49:10 +0530

    Used env variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWGJWNdTzuXcQsgIk4pCBpLibqmH1g1II",
-  authDomain: "gdsc-bc063.firebaseapp.com",
-  projectId: "gdsc-bc063",
-  storageBucket: "gdsc-bc063.appspot.com",
-  messagingSenderId: "46055800489",
-  appId: "1:46055800489:web:220016541777195b84b3b8",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
